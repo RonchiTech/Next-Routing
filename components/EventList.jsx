4ef3732cd0600b30e81709c14fc47929e61deca8
ronchitech <ronchibusiness@gmail.com>
2021-05-10 23:03:32 +0800

    added dummy data and react components

---
+++ components/EventList.jsx
@@ -0,0 +1,9 @@
+import EventItem from './EventItem'
+function EventList({events}) {
+    return (
+        <ul>
+            {events.map(event => <EventItem key={event.id} event={event}/>)}
+        </ul>
+    )
+}
+export default EventList
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import EventList from '../components/EventList'
+import { getFeaturedEvents } from '../dummy'
 function MainPage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
       <h1>Main Page</h1>
+      <EventList events={featuredEvents} />
     </div>
   );
 }
